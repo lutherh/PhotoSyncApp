c88cec0a3af0523de10b9813ea93e805fe31daca
lutherh <hasan.galib.luther@lego.com>
2025-08-29 11:16:09 +0200

    Initial Commit

---
+++ PhotoSyncApp/PhotoSyncAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSyncAppApp.swift
+//  PhotoSyncApp
+//
+//  Created by Hasan Galib Luther on 29/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSyncAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
